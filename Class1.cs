afa39b52add542956482856a620d89d2d1a572ad
Kundan <baidyapradip005@gmail.com>
2023-10-05 07:49:18 +0545

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningdotnet
+{
+    internal class dog
+    {
+        public static string name;
+        public static int age;
+
+        public dog() { 
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningdotnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dog obj = new dog();
+            dog.name = "sylvie";
+            dog.age = 12;
+        }
+    }
+}
